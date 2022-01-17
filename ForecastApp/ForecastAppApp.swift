f69daf58ada948584181afe107d469f1307bb63a
Code Palace <codepalace@fastmail.se>
2022-01-17 19:07:28 +0100

    Initial Commit

---
+++ ForecastApp/ForecastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastAppApp.swift
+//  ForecastApp
+//
+//  Created by Federico on 17/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ForecastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
